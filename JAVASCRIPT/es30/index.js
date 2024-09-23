a951d9da442319b462e76676fb2bd9dc4cc7813b
IlariaM <ilaria.mammana15@gmail.com>
2024-09-23 11:13:31 +0200

    added in-depth analysis to es30

--- JAVASCRIPT/es30/index.js
+++ JAVASCRIPT/es30/index.js
@@ -11,3 +11,23 @@ const words = ["banana", "apple", "cherry", "date"]
 words.sort()
 
 console.log(words);
+
+const numArr = [7, 28, 34, 67, 89, 92]
+
+const sorted = numArr.sort((a, b) => a - b)
+
+console.log(sorted);
+
+const strings = ["suricato", "rinoceronte", "anatra", "cane", "zanzara"]
+
+const sortedStrings = strings.sort()
+
+const sortedReverse = []
+
+for (let index = sortedStrings.length - 1; index >= 0; index--) {
+  sortedReverse.push(sortedStrings[index])
+}
+
+console.log(sorted);
+
+console.log(sortedReverse);

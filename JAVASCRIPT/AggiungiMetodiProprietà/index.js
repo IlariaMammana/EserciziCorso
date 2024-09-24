c4bbccfde3e46516da8b00bf92bd8dd6955db6a6
IlariaM <ilaria.mammana15@gmail.com>
2024-09-24 13:53:36 +0200

    removed index.html

---
+++ JAVASCRIPT/AggiungiMetodiProprietà/index.js
@@ -0,0 +1,14 @@
+class Automobile {
+  marca = ""
+  modello = ""
+  anno = ""
+
+  constructor(marca, modello, anno) {
+    this.marca = marca,
+    this.modello = modello,
+    this.anno = anno
+  }
+  descrizione() {
+    return "Automobile: {this.marca} {this.modello}, Anno: {this.anno}"
+  }
+}

6be95160fd2d68d906aa3d7f92f2c6aad895c7de
IlariaM <ilaria.mammana15@gmail.com>
2024-09-30 10:29:33 +0200

    added DestructuringValoriDefiniti

---
+++ JAVASCRIPT/DestructuringValoriDefiniti/index.js
@@ -0,0 +1,6 @@
+let numbers = [1, 1994, 30, 2024];
+
+
+let [birthMonth, birthYear, courrentAge, courrentYear, birthDay = 15] = numbers
+
+console.log("Nata il giorno " + birthDay + " " + birthMonth + " " + birthYear + ". " + "Ha " + courrentAge + " anni nell'anno " + courrentYear);

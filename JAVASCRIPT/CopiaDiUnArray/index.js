630cad635d8b408a36860a006954fa1185c16168
IlariaM <ilaria.mammana15@gmail.com>
2024-09-30 12:17:46 +0200

    added CopiaDiUnArray

---
+++ JAVASCRIPT/CopiaDiUnArray/index.js
@@ -0,0 +1,10 @@
+let originalNumbers = [1, 1994, 30, 2024, 15];
+
+let copyOriginalNumbers = [...originalNumbers];
+originalNumbers.push(9)
+
+console.log(originalNumbers);
+console.log(copyOriginalNumbers);
+
+
+

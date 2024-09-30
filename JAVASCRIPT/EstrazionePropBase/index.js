f5bb7081f2f8cd0673287c09614017402245de10
IlariaM <ilaria.mammana15@gmail.com>
2024-09-30 11:46:20 +0200

    added EstrazionePropBase

---
+++ JAVASCRIPT/EstrazionePropBase/index.js
@@ -0,0 +1,12 @@
+let student = {
+    name: "Bob",
+    surname: "Donovan",
+    grade: "A",
+    major: "technology"
+};
+
+let {name, surname, major, grade} = student;
+
+console.log("The student " + name + " " + surname + " from the major " + major + ", has the grade: " + grade + ".");
+
+

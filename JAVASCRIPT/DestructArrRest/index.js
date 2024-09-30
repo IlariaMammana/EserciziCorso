c7e8596f7b566dabeb823a797aa50cf285b2f9b7
IlariaM <ilaria.mammana15@gmail.com>
2024-09-30 15:38:14 +0200

    fixed DestructArrRest

--- JAVASCRIPT/DestructArrRest/index.js
+++ JAVASCRIPT/DestructArrRest/index.js
@@ -1,8 +1,9 @@
-let numbers = [15, 1994, 15, 1, 2024];
+let numbers = [15, 1994, 30, 2024, 65];
 
 let [birthDay, birthYear, ...altro] = numbers;
 
-console.log("Se Ilaria è nata il " + birthDay + " gennaio " + birthYear + ": quanti anni avrà il " + altro + "?" );
+console.log(birthDay);
+console.log(birthYear);
+console.log(altro);
 
-// so che è un caso improbabile ma volevo giocarci un po'
 

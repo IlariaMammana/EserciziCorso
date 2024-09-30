d8ba977577da89886866b3e8c80de5a27ca480b4
IlariaM <ilaria.mammana15@gmail.com>
2024-09-30 20:14:56 +0200

    added FromObjToJson

---
+++ JAVASCRIPT/FromObjToJson/index.js
@@ -0,0 +1,13 @@
+let student = {
+    name: "Fred",
+    surname: "Weasley",
+    grade: "5th",
+    house: "Griffyndor"
+}
+
+let studentJson = JSON.stringify(student)
+
+console.log(studentJson);
+
+
+

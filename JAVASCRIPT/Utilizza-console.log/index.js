1656a4cf40ad6925253c280d9fe2d25b9f68564b
IlariaM <ilaria.mammana15@gmail.com>
2024-10-01 09:27:11 +0200

    added Utilizza-console.log

---
+++ JAVASCRIPT/Utilizza-console.log/index.js
@@ -0,0 +1,13 @@
+let numero = 89;
+
+let stringa = "Ammaccabanane";
+
+let array = [1, 2, 3, 4, 5];
+
+let oggetto = {
+    nome: "Miguel",
+    età: 37,
+    città: "New York"
+};
+
+console.log(numero, stringa, array, oggetto);

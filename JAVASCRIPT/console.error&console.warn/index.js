532326d955f2f896bc6a17bb8cc136c6a87e401c
IlariaM <ilaria.mammana15@gmail.com>
2024-10-01 09:48:56 +0200

    added console.error&console.warn

---
+++ JAVASCRIPT/console.error&console.warn/index.js
@@ -0,0 +1,8 @@
+let age = 0;
+if (age === 0) {
+  console.error("Age must be declared.");
+} else if ( age >= 1 , age < 18) {
+    console.warn("User is under 18 years old. Proceed with caution.")
+} else {
+    console.log("Proceed.")
+}

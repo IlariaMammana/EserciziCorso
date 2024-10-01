0f5e67d9479944782508c968d6721affc9e8daa0
IlariaM <ilaria.mammana15@gmail.com>
2024-10-01 10:02:54 +0200

    added console.table&console.group

---
+++ JAVASCRIPT/console.table&console.group/index.js
@@ -0,0 +1,35 @@
+let weasley = [
+    {
+      name: "Charlie",
+      surname: "Weasley",
+      house: "Griffyndor"
+    },
+    {
+      name: "Percy",
+      surname: "Weasley",
+      house: "Griffyndor"
+    },
+    {
+      name: "George",
+      surname: "Weasley",
+      house: "Griffyndor"
+    },
+    {
+      name: "Fred",
+      surname: "Weasley",
+      house: "Griffyndor"
+    },
+    {
+      name: "Ronald",
+      surname: "Weasley",
+      house: "Griffyndor"
+    },
+    {
+      name: "Ginny",
+      surname: "Weasley",
+      house: "Griffyndor"
+    }
+  ];
+
+console.group("Weasley siblings");
+console.table(weasley);

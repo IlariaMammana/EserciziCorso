12925d0786d23040409bf875390e4b2df8e38574
IlariaM <ilaria.mammana15@gmail.com>
2024-10-07 21:36:43 +0200

    added CallbackBase

---
+++ JAVASCRIPT/CallbackBase/index.js
@@ -0,0 +1,14 @@
+ 
+function sumCallback(num1, num2, callback) {
+    const result = num1 + num2;
+    console.log(result);
+    callback(result);
+}
+
+function myCallback(result) {
+    let newResult = result *2;
+    console.log("Il nuovo risultato è: " + newResult);
+    
+}
+
+sumCallback(5, 3, myCallback);

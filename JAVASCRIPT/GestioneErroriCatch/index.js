ec93d4ba2db10cf2437ea3225aa171805a14b3a2
IlariaM <ilaria.mammana15@gmail.com>
2024-10-08 17:25:32 +0200

    added new  excersise and fixed another

--- JAVASCRIPT/GestioneErroriCatch/index.js
+++ JAVASCRIPT/GestioneErroriCatch/index.js
@@ -1,21 +1,10 @@
-function numAfterOneSec() {
-    return new Promise((resolve, reject) => {
-        const num = Math.floor(Math.random() * 100);
-        if (num >= 50) {
-            resolve(num);
-        } else {
-            reject(`Il numero è minore di 50`);
-        };
-    });
+function errorFunction() {
+    return new Promise((_, reject) => {
+        reject(`Errore nell'esecuzione.`);
+    })
 }
 
-numAfterOneSec(true)
-    .then((num) => {
-        return num + 1;
-    })
-    .then((result) => {
-        console.log(`Il risultato è ${result}`);
-    })
+errorFunction()
     .catch(error => {
         console.error(error);
         console.log("Gestione dell'errore completata.");

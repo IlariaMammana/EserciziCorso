21d0b0a29f2fa1b350f775e81cd4f18633570840
IlariaM <ilaria.mammana15@gmail.com>
2024-10-08 16:51:03 +0200

    added PromiseChainCondiz

---
+++ JAVASCRIPT/PromiseChainCondiz/index.js
@@ -0,0 +1,30 @@
+function numAfterOneSec(check) {
+    return new Promise((resolve) => {
+        console.log("Verifica 1s");
+        setTimeout(() => {
+            if (check) {
+                const num = Math.floor(Math.random() * 100);
+                // Ho cercato : come generare numeri random da 0 a 100
+                // trovato qui: https://www.codingcreativo.it/math-random-in-javascript/#:~:text=Quindi%20per%20generare%20i%20numeri,poi%20con%20la%20funzione%20Math.
+                resolve(num);
+            } else {
+                reject("Errore nell'esecuzione")
+            }
+        }, 1000);
+    });
+}
+
+numAfterOneSec(true)
+    .then((num) => {
+        if (num % 2 === 0) {
+            return num * 5;
+        } else {
+            return num + 1;
+        }
+    })
+    .then((result) => {
+        console.log(`Il risultato è ${result}`);
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });

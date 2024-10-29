e6e41c18339e8a02f5c28863bc9e9fbd71552e88
IlariaM <ilaria.mammana15@gmail.com>
2024-10-29 10:49:40 +0100

    fixed DarkMode

--- TAILWIND/DarkMode/script.js
+++
@@ -1,6 +0,0 @@
-const toggleDarkMode = () => {
-    document.body.classList.toggle("dark");
-    document.querySelector("#darkModeStatus").innerHTML = document.body.classList.contains("dark")? "Active" : "Inactive";
-}
-
-document.querySelector("#darkModeToggle").addEventListener("click", toggleDarkMode)

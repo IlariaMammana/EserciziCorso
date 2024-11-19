dcbc79827ad7cdc0ef8cae918b4737db37113488
IlariaM <ilaria.mammana15@gmail.com>
2024-11-19 12:59:18 +0100

    used useNavigate

--- REACT/new-project/src/App.jsx
+++ REACT/new-project/src/App.jsx
@@ -3,13 +3,11 @@ import { Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import About from "./pages/About"
 
-import Navbar from "./components/Navbar"
 import PublicLayout from './layouts/PublicLayout'
 
 const App = () => {
     return (
         <>
-            <Navbar />
             <Routes>
                 <Route path='/' element={<PublicLayout />}>
                     <Route path="" element={<Home />} />
--- REACT/new-project/src/pages/About.jsx
+++ REACT/new-project/src/pages/About.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 const About = () => {
+  const navigate = useNavigate()
+
+  const handleNavigate = () => {
+    navigate('/')
+  }
+
   return (
     <>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rerum perspiciatis, quaerat itaque, dicta recusandae doloremque in quo eligendi voluptatem officia. Omnis reiciendis aspernatur perspiciatis soluta, at ipsam libero quasi.</p>
+      <button onClick={handleNavigate}>Torna a Home</button>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum rerum perspiciatis, quaerat itaque, dicta recusandae doloremque in quo eligendi voluptatem officia. Omnis reiciendis aspernatur perspiciatis soluta, at ipsam libero quasi.</p>
     </>
   )
 }

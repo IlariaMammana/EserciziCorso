678517dfbc44cb9fb933efececa0bca99e3f9300
IlariaM <ilaria.mammana15@gmail.com>
2024-11-19 12:40:48 +0100

    added new Layout

--- REACT/new-project/src/App.jsx
+++ REACT/new-project/src/App.jsx
@@ -4,14 +4,17 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 
 import Navbar from "./components/Navbar"
+import PublicLayout from './layouts/PublicLayout'
 
 const App = () => {
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route path='/' element={<PublicLayout />}>
+                    <Route path="" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                </Route>
             </Routes>
         </>
     )
